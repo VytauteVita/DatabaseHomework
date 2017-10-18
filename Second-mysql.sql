210829a528698fa9731db55ec306e6f1638f9474
VytauteVita <VytauteVita@users.noreply.github.com>
2017-10-18 19:14:25 +0300

    4.b

--- Second-mysql.sql
+++ Second-mysql.sql
@@ -1,2 +1,11 @@
 ALTER TABLE `Books` ADD Genre text;
 
+CREATE TABLE AB
+(
+    `authorId` int NOT NULL,
+    `bookId` int NOT NULL);
+
+INSERT INTO `AB` (`authorId`,`bookId`) VALUES
+(1, 1),
+(2, 2),
+(4, 3);
