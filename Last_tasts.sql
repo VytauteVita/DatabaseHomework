f44e5176660e48fdbcfcaa72f71782566a25ffc3
VytauteVita <VytauteVita@users.noreply.github.com>
2017-10-18 21:14:58 +0300

    Second task

--- Last_tasts.sql
+++ Last_tasts.sql
@@ -1,1 +1,6 @@
 SELECT DISTINCT userId FROM `UserClicks` WHERE `dateTime` >= "2017-01-01";
+
+SELECT *, floor(datediff(curdate(),birthDate) / 365) 
+FROM Clients
+WHERE 
+         DATE_FORMAT(birthDate,'%m-%d') = DATE_FORMAT(NOW(),'%m-%d');
