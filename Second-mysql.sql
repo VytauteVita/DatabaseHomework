5d061847783ad8e4743a759b1c07303c59e1abfe
VytauteVita <VytauteVita@users.noreply.github.com>
2017-10-18 19:40:46 +0300

    4.d

--- Second-mysql.sql
+++ Second-mysql.sql
@@ -8,4 +8,9 @@ CREATE TABLE AB
 INSERT INTO `AB` (`authorId`,`bookId`) VALUES
 (1, 1),
 (2, 2),
-(4, 3);
+(4, 3),
+(15,3);
+
+SELECT * FROM `Books` LEFT JOIN `AB` ON `Books`.`bookId` = `AB`.`bookId` LEFT JOIN `Authors` ON `AB`. `authorId` = `Authors`.`authorId` ;
+
+
