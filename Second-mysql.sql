2be5e0bb40a815dddacc4f70f233b4c6972d1fcb
VytauteVita <VytauteVita@users.noreply.github.com>
2017-10-18 20:00:11 +0300

    4.a

--- Second-mysql.sql
+++ Second-mysql.sql
@@ -13,4 +13,12 @@ INSERT INTO `AB` (`authorId`,`bookId`) VALUES
 
 SELECT * FROM `Books` LEFT JOIN `AB` ON `Books`.`bookId` = `AB`.`bookId` LEFT JOIN `Authors` ON `AB`. `authorId` = `Authors`.`authorId` ;
 
+CREATE TABLE IF NOT EXISTS `Genres` (
+  `genreId` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET latin1 COLLATE latin1_german1_ci NOT NULL,
+  PRIMARY KEY (`genreId`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 ;
 
+ALTER TABLE `Books` DROP COLUMN Genre;
+ALTER TABLE `Books` DROP COLUMN `authorId`;
+ALTER TABLE `Books` ADD `genreId` int;
